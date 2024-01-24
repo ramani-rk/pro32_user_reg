1a927bf43cc1d081fbd1f24c18e61238e2cf321a
ramani-rk <ramanikanthsasi@gmail.com>
2024-01-24 19:59:56 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,10 +4,12 @@ from django.shortcuts import render
 
 
 from app.forms import *
-from django.http import HttpResponse,HttpResponseRedirect
-from django.contrib.auth import authenticate,login
-from django.core.mail import send_mail
+from app.models import *
 from django.urls import reverse
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse,HttpResponseRedirect
 
 def registration (request):
     ufo=UserForm()
@@ -22,12 +24,14 @@ def registration (request):
 
 
 # To convert Non-Modified Function Data object into Modified Function Data object (2)
+    #userform module
         if ufd.is_valid() and pfd.is_valid():
             MUFDO=ufd.save(commit=False)
             pw=ufd.cleaned_data['password']
             MUFDO.set_password(pw)
             MUFDO.save()
 
+    #profile module
             MPFDO=pfd.save(commit=False)
             MPFDO.username=MUFDO
             MPFDO.save()
@@ -54,13 +58,13 @@ def registration (request):
                 fail_silently = False
                 )
 
-#-----------------------------------------------------------------------#
-
-
             return HttpResponse('Registration is Sucessfull')
 
         else:
             return HttpResponse('Invalid data')
+
+#-----------------------------------------------------------------------#
+      
     return render (request,'registration.html',d)
 
 #-----------------------------------------------------------------------#
@@ -78,6 +82,8 @@ def home(request):
 #-----------------------------------------------------------------------#
 # Login
 
+
+
 def user_login(request):
     if request.method=='POST':
         username=request.POST['un']
@@ -94,3 +100,24 @@ def user_login(request):
     return render(request,'user_login.html')
 
 
+#-----------------------------------------------------------------------#
+# Logout
+
+@login_required
+def user_logout (request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
+#-----------------------------------------------------------------------#
+# Display the User Details
+
+
+@login_required
+def profile_display (request):
+    un=request.session.get('username')
+    uo=User.objects.get(username=un)
+    po=Profile.objects.get(username=uo)
+    d={'uo':uo,'po':po}
+
+    return render(request,'profile_display.html',d)
--- pro32userreg/urls.py
+++ pro32userreg/urls.py
@@ -28,5 +28,7 @@ urlpatterns = [
     path('registration/',registration,name='registration'),
     path('user_login/',user_login,name='user_login'),
     path('home/',home,name='home'),
+    path('user_logout/',user_logout,name='user_logout'),
+    path('profile_display/',profile_display,name='profile_display'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
